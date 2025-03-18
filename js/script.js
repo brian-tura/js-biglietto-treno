62738c2c0f23d3f75b9a9ac5e837b0568b544051
brian-tura <b.tura2002@gmail.com>
2025-03-18 06:07:06 +0100

    Excercise done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let dist = prompt("Please enter the desired distance");
 let age = prompt("Please enter your age");
 let price;
-
+let roundedPrice;
 const ticketPrice = 0.21;
 
 if(age < 18) {
@@ -12,6 +12,6 @@ if(age >= 65){
     price = (ticketPrice * dist) * (6/10);
 }
 
+roundedPrice = price.toFixed(2);
 
-
-console.log(price);
+console.log(roundedPrice);
