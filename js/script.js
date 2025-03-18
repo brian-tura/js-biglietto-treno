2e83282082090d71af2f7c9c0f235d7ccdbbaef1
brian-tura <b.tura2002@gmail.com>
2025-03-18 06:00:33 +0100

    Added minor discount

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 let dist = prompt("Please enter the desired distance");
 let age = prompt("Please enter your age");
+let price
 const ticketPrice = 0.21;
+
+if(age < 18) {
+    price = (ticketPrice * dist) * (8/10);
+}
+
+console.log(price)
